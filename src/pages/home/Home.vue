9d7cd5c821dc1b36a84d56eae966e79c0a163c7c
syy1234 <2239618177@qq.com>
2019-03-19 21:50:34 +0800

    定义header组件

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,9 +1,17 @@
 <template>
-
+    <div>
+			<home-header></home-header>
+		</div>
 </template>
 
 <script>
-
+import HomeHeader from './component/Header'
+export default {
+	name: 'HomeIndex',
+	components: {
+		HomeHeader: HomeHeader
+	}
+}
 </script>
 
 <style>
---
+++ src/pages/home/component/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="header">我是header</div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style scoped>
+.header{
+	width:100%;
+	height:.86rem;
+	background:#eeeeee;
+}
+</style>
